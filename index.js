102bedd931479554fa475c106e3b8e6d48116826
MateusCE <contato.mateusoliveira1993@gmail.com>
2024-11-05 06:14:46 -0300

    Landing page atualizada

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-var SetaDireita = window.document.getElementById("seta-direta")
+var setaDireita = window.document.getElementById("seta-direita")
 var Leonardo = window.document.getElementById("Leonardo")
 var Samantha = window.document.getElementById("Samantha")
 var Bruna = window.document.getElementById("Bruna")
-var SetaEsquerda = window.document.getElementById("seta-esquerda")
-
+var setaEsquerda = window.document.getElementById("seta-esquerda")
 function RolarParaDireita() {
-    Leonardo.style = "display:none"
-    Bruna.style = "display:flex"
-    SetaDireita.style = "display:none"
-    SetaEsquerda.style = "displey:flex; margin-top:55px"
+    Leonardo.style ="display:none"
+    Bruna.style ="display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display:flex; margin-top:px"
 }
-
 function RolarParaEsquerda() {
-    Leonardo.style = "display: flex"
-    Samantha.style - "display: flex"
-    Bruna.style = "display: none"
-    SetaEsquerda.style = "displey:flex; margin-top:55px"
-    SetaDireita.style = "display:none"
+    Leonardo.style ="display:flex"
+    Bruna.style ="display:none"
+    setaDireita.style = "display:flex"
+    setaEsquerda.style = "display:none"
+    setaDireita.style = "display:flex; margin-top:55px"
 }
